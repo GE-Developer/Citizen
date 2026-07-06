ef2b44f003d455dee48d4a78dc27fb995b38f08d
GE-Developer <mikhail.bukhrashvili@gmail.com>
2026-07-06 04:12:48 +0400

    Code refactoring

--- Citizen/App/HomeView.swift
+++ Citizen/App/HomeView.swift
@@ -7,64 +7,101 @@
 
 import SwiftUI
 
-import SwiftUI
-
 struct HomeView: View {
-    @StateObject private var tabBarState = TabBarState()
     @StateObject private var store = StoreManager()
     
-//    private let accent = AccentColorManager.shared
-//    private let screenshotProtector = ScreenshotManager.shared
+    @State private var tabBarState = TabBarState()
+    @State private var languageManager = LanguageManager.shared
     
-    var body: some View {
-        TabView(selection: $tabBarState.selectedTab) {
-            NavigationStack {
-                LearnView()
-            }
-            .toolbar(.hidden, for: .tabBar)
-            .tag(TabBarState.RootTab.home)
-
-            NavigationStack {
-                SettingsView()
-            }
-            .toolbar(.hidden, for: .tabBar)
-            .tag(TabBarState.RootTab.settings)
-        }
-        .safeAreaInset(edge: .bottom) {
-            CustomTabBar()
-        }
-//        .task { setAccentColorIfPremiumExpired() }
-//        .task { await setIconIfPremiumExpired() }
-//        .task { setCaptureProtectionIfPremiumExpired() }
-        .preferredColorScheme(ThemeManager.shared.theme)
-//        .screenshotDisabled(screenshotProtector.isScreenshotProtectionOn)
-        .environmentObject(tabBarState)
-        .environmentObject(store)
+    private var layoutDirection: LayoutDirection {
+        let rtlLanguages = Language.rtlLanguages
+        return rtlLanguages.contains(languageManager.currentLanguageID) ? .rightToLeft : .leftToRight
     }
     
-//    private func setAccentColorIfPremiumExpired() {
-//        guard !store.isPremium else { return }
-//        if accent.currentColor != .midnightBlue {
-//            accent.currentColor = .midnightBlue
-//        }
-//    }
+    private let accent = AccentColorManager.shared
+    private let screenshotProtector = ScreenshotManager.shared
     
-//    private func setIconIfPremiumExpired() async {
-//        guard !store.isPremium else { return }
-//        if AppIconManager.currentIcon() != .blackVoid {
-//            do {
-//                try await Task.sleep(for: .milliseconds(600))
-//                try await AppIconManager.setIcon(.blackVoid)
-//            } catch {
-//                return
-//            }
-//        }
-//    }
-//    
-//    private func setCaptureProtectionIfPremiumExpired() {
-//        guard !store.isPremium else { return }
-//        if screenshotProtector.isScreenshotProtectionOn {
-//            screenshotProtector.isScreenshotProtectionOn = false
-//        }
-//    }
+    var body: some View {
+        homeView
+            .safeAreaInset(edge: .bottom) {
+                CustomTabBar()
+            }
+            .ignoresSafeArea(.keyboard, edges: .bottom)
+            .task { setAccentColorIfPremiumExpired() }
+        //            .task { await setIconIfPremiumExpired() }
+        //            .task { setCaptureProtectionIfPremiumExpired() }
+            .environment(\.layoutDirection, layoutDirection)
+            .preferredColorScheme(ThemeManager.shared.theme)
+            .screenshotDisabled(screenshotProtector.isScreenshotProtectionOn)
+            .environment(tabBarState)
+            .environment(languageManager)
+            .environmentObject(store)
+    }
+}
+
+// MARK: - Builder
+extension HomeView {
+    private var homeView: some View {
+        TabView(selection: $tabBarState.selectedTab) {
+            dictionary
+            home
+            settings
+        }
+    }
+    
+    private var dictionary: some View {
+        NavigationStack {
+            DictionaryView()
+        }
+        .environment(\.parentTab, .dictionary)
+        .toolbar(.hidden, for: .tabBar)
+        .tag(RootTab.dictionary)
+    }
+    
+    private var home: some View {
+        NavigationStack {
+            MainView()
+        }
+        .environment(\.parentTab, .home)
+        .toolbar(.hidden, for: .tabBar)
+        .tag(RootTab.home)
+    }
+    
+    private var settings: some View {
+        NavigationStack {
+            SettingsView()
+        }
+        .environment(\.parentTab, .settings)
+        .toolbar(.hidden, for: .tabBar)
+        .tag(RootTab.settings)
+    }
+}
+
+// MARK: - Logic
+extension HomeView {
+    private func setAccentColorIfPremiumExpired() {
+        guard !store.isPremium else { return }
+        if accent.currentColor != .georgian {
+            accent.currentColor = .georgian
+        }
+    }
+    
+    private func setIconIfPremiumExpired() async {
+        guard !store.isPremium else { return }
+        if AppIconManager.currentIcon() != .blackCitizen {
+            do {
+                try await Task.sleep(for: .milliseconds(600))
+                try await AppIconManager.setIcon(.blackCitizen)
+            } catch {
+                return
+            }
+        }
+    }
+    
+    private func setCaptureProtectionIfPremiumExpired() {
+        guard !store.isPremium else { return }
+        if screenshotProtector.isScreenshotProtectionOn {
+            screenshotProtector.isScreenshotProtectionOn = false
+        }
+    }
 }
